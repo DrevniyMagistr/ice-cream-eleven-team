d36df12c09bcf538ee1ffce78c549afd1cc377bd
DrevniyMagistr <43793076+DrevniyMagistr@users.noreply.github.com>
2021-11-16 21:58:28 +0200

    Update header-fixed.js

--- src/js/header-fixed.js
+++ src/js/header-fixed.js
@@ -4,3 +4,21 @@ $(function () {
 	  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 	});
 });
+
+
+const smoothAnchors = document.querySelectorAll('.menu__link')
+const header= document.querySelector('.header-js')
+for (let anchor of smoothAnchors) {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    const anchorID = anchor.getAttribute('href')
+    const scrollOffset = document.querySelector(anchorID).offsetTop
+    window.scrollTo({
+      top: scrollOffset - 80,
+      behavior: 'smooth',
+    })
+  })
+}
+$(window).on('scroll', function () {
+  $(header).toggleClass('fixed', $(this).scrollTop() > $('.hero-js').height());
+});
