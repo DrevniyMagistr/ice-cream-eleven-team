42c2b9adb23aedee73cdf75f65e03a039ac18ad1
DrevniyMagistr <sasha.klishch@gmail.com>
2021-11-18 14:30:50 +0200

    add animation

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,10 @@
 //$('.single-item').slick();
 $('.review-js').slick({
   dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
   infinite: true,
-  speed: 300,
+  speed: 1000,
     slidesToShow: 1,
   arrows: false
 });
